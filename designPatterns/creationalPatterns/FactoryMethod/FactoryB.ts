3da264a96961fc243253c72b7b74c7d392231178
Fab <fabou291@gmail.com>
2022-11-01 18:57:30 +0100

    🩹 fix irrelevant name given to factory object of factory method pattern Fixes #4

--- designPatterns/creationalPatterns/FactoryMethod/FactoryB.ts
+++ designPatterns/creationalPatterns/FactoryMethod/FactoryB.ts
@@ -1,8 +1,8 @@
-import Logistics from './Factory';
+import Factory from './Factory';
 import ProductB from './ProductB';
 import Product from './Product';
 
-export default class FactoryB extends Logistics {
+export default class FactoryB extends Factory {
   // eslint-disable-next-line
   public createProduct(): Product {
     return new ProductB();
